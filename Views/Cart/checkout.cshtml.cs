e572d9ea2ae09f58ee658b06db0e78e847d0e6c6
Jali Suhaman <86903939+Jalil98@users.noreply.github.com>
2025-07-14 17:07:13 +0700

    Add files via upload

---
+++ Views/Cart/checkout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KatalogProduk.Views.Cart
+{
+    public class checkoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
